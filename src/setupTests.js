085c4b68b8cd5a73bebd829fa7772fe60a4f49f6
BAD <144672937+BDJAHNIT@users.noreply.github.com>
2024-06-20 07:09:29 +0200

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,8 @@
-// jest-dom ajoute des comparateurs personnalisés pour l'insertion sur le DOM nodes.
+// jest-dom ajoute des comparateurs personnalisés pour l'insertion sur DOM node.
 
 // vous permet de faire des choses comme :
 
-// attendre(élément).toHaveTextContent(/react/i)
+// await(element).toHaveTextContent(/react/i)
 
 // en savoir plus : https://github.com/testing-library/jest-dom
 
