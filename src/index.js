cd11a006886fc1a01d46c337b3586048a7afc840
BAD <144672937+BDJAHNIT@users.noreply.github.com>
2024-06-20 07:06:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,11 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Si vous souhaitez commencer à mesurer les performances de votre application, transmettez une fonction
-
-// pour enregistrer les résultats (par exemple : reportWebVitals(console.log))
-
-// ou envoyer à un point de terminaison d'analyse. En savoir plus : https://bit.ly/CRA-vitals
+ 
 
 reportWebVitals();
