f9e4548b2d99fe8eddf7be14eade54e223e58ee7
BAD <144672937+BDJAHNIT@users.noreply.github.com>
2024-06-20 06:39:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,29 +24,29 @@ contactEmail.verify((error) => {
   if (error) {
     console.log(error);
   } else {
-    console.log("Ready to Send");
+    console.log("Prêt à envoyer");
   }
 });
 
 router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
+  const name = req.body.prénom + req.body.nom;
   const email = req.body.email;
   const message = req.body.message;
-  const phone = req.body.phone;
+  const téléphone = req.body.téléphone;
   const mail = {
-    from: name,
-    to: "b.djahnit@gmail.com",
+    from: nom,
+    to: "*****@gmail.com",
     subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
+    html: `<p>Nom: ${nom}</p>
            <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
+           <p>Téléphone: ${téléphone}</p>
            <p>Message: ${message}</p>`,
   };
   contactEmail.sendMail(mail, (error) => {
     if (error) {
       res.json(error);
     } else {
-      res.json({ code: 200, status: "Message Sent" });
+      res.json({ code: 200, status: "Message envoyer" });
     }
   });
 });
